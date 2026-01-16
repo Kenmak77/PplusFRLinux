3093d8d370ccff5a5c5ceb0d757e443ed43c5c0a
Kenmak77 <xelradark@gmail.com>
2026-01-16 02:47:38 +0100

    Update P+FR_AutoUpdate.sh

--- P+FR_AutoUpdate.sh
+++ P+FR_AutoUpdate.sh
@@ -5,10 +5,10 @@
 # ======================================================
 # Compatible : Ubuntu, Linux Mint, Arch, Manjaro, Fedora
 # Author : Kenmak77
-# Version : 2.6.2
+# Version : 2.6.3
 #
 # CHANGELOG
-# v2.6.2
+# v2.6.3
 # - Téléchargement SD multi-méthode (aria2c → rclone → wget)
 # - AppImage & ZIP forcés en HTTP (wget)
 # - SD téléchargée avant AppImage
@@ -41,7 +41,7 @@ fi
 # -----------------------
 # 🔧 CONFIGURATION DE BASE
 # -----------------------
-SCRIPT_VERSION="2.6.2"
+SCRIPT_VERSION="2.6.3"
 
 INSTALL_DIR="$HOME/.local/share/P+FR"
 APPIMAGE_PATH="$INSTALL_DIR/P+FR.AppImage"
