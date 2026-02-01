2d65e21eb54a9f8f3a9c6e375099874fb00524c9
Kenmak77 <xelradark@gmail.com>
2026-02-01 15:52:01 +0100

    Update P+FR_AutoUpdate.sh

--- P+FR_AutoUpdate.sh
+++ P+FR_AutoUpdate.sh
@@ -41,7 +41,7 @@ fi
 # -----------------------
 # 🔧 CONFIGURATION DE BASE
 # -----------------------
-SCRIPT_VERSION="2.7.8"
+SCRIPT_VERSION="2.7.9"
 
 INSTALL_DIR="$HOME/.local/share/P+FR"
 APPIMAGE_PATH="$INSTALL_DIR/P+FR.AppImage"
@@ -272,8 +272,9 @@ extract_zip() {
     
     mv "$INSTALL_DIR/unzipped/Ishiiruka P+FR/User/Wii/title" "$INSTALL_DIR/Ishiiruka/Wii/" 2>/dev/null || true
     
-    ln -s "$INSTALL_DIR/unzipped/User/Load/" "$INSTALL_DIR/Ishiiruka/" 
+    ln -s "$INSTALL_DIR/Load/Textures/RSBE01/" "$INSTALL_DIR/Ishiiruka/Load/Textures/" 
     ln -s "$INSTALL_DIR/Wii/sd.raw" "$INSTALL_DIR/Ishiiruka/Wii/"
+    ln -s "$INSTALL_DIR/Wii/" "$INSTALL_DIR/User/"
     
     rm -rf "$INSTALL_DIR/unzipped"
     rm -f "$ZIP_PATH"
